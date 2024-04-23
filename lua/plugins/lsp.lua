ab6f412bc323ea9c3aed5f9af5728e5638a16260
Vincent Cradler <vincent.cradler@workday.com>
2024-04-23 13:49:35 -0700

    updated python ft file and added defaults for lspconfig --highly recommend btw

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -24,10 +24,24 @@ return {
       end
 
       local lspconfig = require("lspconfig")
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      -- currently managed by ASDF
       lspconfig.gopls.setup{
       }
+      vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          local opts = { buffer = ev.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<leader>sh', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+        end
+      })
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      -- currently managed by ASDF
 
       local linters = {
         "pylint",
