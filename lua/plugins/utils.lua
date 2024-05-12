84209a32e6ad5f9bb6efc42605372a93afb8fd5f
Vincent Cradler <vincent.cradler@workday.com>
2024-05-11 22:10:42 -0700

    added function to test if Jq is installed if so then we add the Jq package

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,3 +1,12 @@
+-- do we have jq installed?
+local have_jq = function()
+  if (os.execute('jq --help > /dev/null 2>&1') == 0) then
+    return {'jrop/jq.nvim'}
+  else
+    return {}
+  end
+end
+
 return {
   {"kwkarlwang/bufresize.nvim", config = function()
     local opts = { noremap=true, silent=true}
@@ -45,7 +54,5 @@ return {
     end,
     build = function() vim.fn["mkdp#util#install"]() end,
   },
-  {
-    'jrop/jq.nvim'
-  },
+  have_jq()
 }
