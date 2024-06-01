cca502c2b1ef2918d462431dcd4e08eed76c4ba1
Vincent Cradler <vincent.cradler@workday.com>
2024-06-01 16:28:49 -0700

    added sesions key configs

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local home = os.getenv("HOME")
 vim.g.mapleader = " "
 
 vim.o.relativenumber = true
@@ -6,21 +7,25 @@ vim.o.expandtab = true
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
 vim.o.smartcase = true
+vim.o.wildmenu = 'full'
 
 vim.cmd [[ set mouse=a ]]
-
-vim.keymap.set('n', '+', '<C-a>')
-vim.keymap.set('n', '-', '<C-x>')
-vim.keymap.set('n', '<leader>tn', ':tabnew<CR>')
 
 vim.opt.wildignore:append { '*/node_modules*' }
 vim.opt.hlsearch = true
 vim.opt.clipboard:append {'unnamed'}
 vim.g.doge_enable_mappings = 0
+vim.g.session_dir = home .. '/.config/nvim/sessions/'
+
+vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>')
+vim.keymap.set('n', '<leader>ss', ':mks ' .. vim.g.session_dir .. vim.fn.expand('%:r') .. '.vim')
+vim.keymap.set('n', '<leader>sr', ':so ' .. vim.g.session_dir)
+
 
 -- :h option-list
 -- :h E355
-local home = os.getenv("HOME")
 vim.o.directory = home .. '/.config/nvim/swapfiles/'
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
