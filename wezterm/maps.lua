5a671da83398b6efaed601037d43b6ccb5e99276
Vincent Cradler <corvi@Vincents-MBP.hsd1.ca.comcast.net>
2025-04-30 09:25:23 -0700

    updated wezterm config to be more decoupled

---
+++ wezterm/maps.lua
@@ -0,0 +1,94 @@
+-- fmt
+local wezterm = require("wezterm") --[[@as Wezterm]]
+local resurrect = require("resurrect")
+
+keys = {
+  {
+    key = "!",
+    mods = "LEADER|SHIFT",
+    action = wezterm.action_callback(function(win, pane)
+      pane:move_to_new_tab()
+    end),
+  },
+  { key = "c", mods = "LEADER", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+  { key = "|", mods = "LEADER", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+  { key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+  { key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  { key = "LeftArrow", mods = "CMD|SHIFT", action = wezterm.action.ActivateTabRelative(-1) },
+  { key = "RightArrow", mods = "CMD|SHIFT", action = wezterm.action.ActivateTabRelative(1) },
+  { key = "n", mods = "LEADER", action = wezterm.action.ActivateTabRelative(1) },
+  { key = "p", mods = "LEADER", action = wezterm.action.ActivateTabRelative(-1) },
+  { key = "z", mods = "LEADER", action = wezterm.action.TogglePaneZoomState },
+  { key = "[", mods = "LEADER", action = wezterm.action.ActivateCopyMode },
+  { key = "j", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Down") },
+  { key = "k", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Up") },
+  { key = "l", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Right") },
+  { key = "l", mods = "CTRL", action = wezterm.action.ShowLauncher },
+  { key = "h", mods = "LEADER", action = wezterm.action.ActivatePaneDirection("Left") },
+  {
+    key = ",",
+    mods = "LEADER",
+    action = wezterm.action.PromptInputLine({
+      description = "Enter new name for tab",
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    }),
+  },
+  {
+    key = "s",
+    mods = "LEADER",
+    action = wezterm.action_callback(function(win, pane)
+      workspace_switcher.switch_workspace()
+    end),
+  },
+  {
+    key = "s",
+    mods = "LEADER|CTRL",
+    action = wezterm.action.EmitEvent("trigger-vim-with-scrollback"),
+  },
+
+  {
+    key = "r",
+    mods = "LEADER",
+    action = wezterm.action.PromptInputLine({
+      description = "Enter new name for workspace",
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          wezterm.mux.rename_workspace(wezterm.mux.get_active_workspace(), line)
+        end
+      end),
+    }),
+  },
+  {
+    key = "r",
+    mods = "LEADER|CTRL",
+    action = wezterm.action_callback(function(win, pane)
+      resurrect.resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
+        local type = string.match(id, "^([^/]+)") -- match before '/'
+        id = string.match(id, "([^/]+)$") -- match after '/'
+        id = string.match(id, "(.+)%..+$") -- remove file extention
+        local opts = {
+          relative = true,
+          restore_text = true,
+          on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+        }
+        if type == "workspace" then
+          local state = resurrect.state_manager.load_state(id, "workspace")
+          resurrect.workspace_state.restore_workspace(state, opts)
+        elseif type == "window" then
+          local state = resurrect.state_manager.load_state(id, "window")
+          resurrect.window_state.restore_window(pane:window(), state, opts)
+        elseif type == "tab" then
+          local state = resurrect.state_manager.load_state(id, "tab")
+          resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+        end
+      end)
+    end),
+  },
+}
+return keys
---
+++ wezterm/resurrect.lua
@@ -0,0 +1,93 @@
+-- fmt
+local wezterm = require("wezterm")
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
+
+local colors = require("colors")
+local os = require("os")
+local io = require("io")
+
+resurrect.state_manager.periodic_save({
+  interval_seconds = 15 * 60,
+  save_workspaces = true,
+  save_windows = true,
+  save_tabs = true,
+})
+
+wezterm.on("resurrect.error", function(err)
+  wezterm.log_error("ERROR!")
+  wezterm.gui.gui_windows()[1]:toast_notification("resurrect", err, nil, 3000)
+end)
+
+workspace_switcher.workspace_formatter = function(label)
+  return wezterm.format({
+    { Attribute = { Italic = true } },
+    { Foreground = { Color = colors.colors.ansi[3] } },
+    { Background = { Color = colors.colors.background } },
+    { Text = "󱂬 : " .. label },
+  })
+end
+
+wezterm.on("trigger-vim-with-scrollback", function(window, pane)
+  local text = pane:get_lines_as_text(pane:get_dimensions().scrollback_rows)
+  local name = os.tmpname()
+  local f = io.open(name, "w+")
+  f:write(text)
+  f:flush()
+  f:close()
+  window:perform_action(
+    wezterm.action.SpawnCommandInNewWindow({
+      args = { "vim", name },
+    }),
+    pane
+  )
+  wezterm.sleep_ms(1000)
+  os.remove(name)
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+  window:gui_window():set_right_status(wezterm.format({
+    { Attribute = { Intensity = "Bold" } },
+    { Foreground = { Color = colors.colors.ansi[5] } },
+    { Text = basename(path) .. "  " },
+  }))
+  local workspace_state = resurrect.workspace_state
+
+  workspace_state.restore_workspace(resurrect.state_manager.load_state(label, "workspace"), {
+    window = window,
+    relative = true,
+    restore_text = true,
+
+    resize_window = false,
+    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+  })
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.chosen", function(window, path, label)
+  wezterm.log_info(window)
+  window:gui_window():set_right_status(wezterm.format({
+    { Attribute = { Intensity = "Bold" } },
+    { Foreground = { Color = colors.colors.ansi[5] } },
+    { Text = basename(path) .. "  " },
+  }))
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+  wezterm.log_info(window)
+  local workspace_state = resurrect.workspace_state
+  resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+  resurrect.state_manager.write_current_state(label, "workspace")
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.start", function(window, _)
+  wezterm.log_info(window)
+end)
+
+wezterm.on("smart_workspace_switcher.workspace_switcher.canceled", function(window, _)
+  wezterm.log_info(window)
+end)
+
+return {
+  resurrect = resurrect,
+  workspace_switcher = workspace_switcher,
+}
