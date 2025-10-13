f7820256e49fafad6126c75245b98dee1ccb431a
Vincent Cradler <vincentcradler@gmail.com>
2025-10-12 23:23:29 -0700

    Added mini.ai to minifile

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -1,48 +1,58 @@
 return {
-  "echasnovski/mini.files",
-  commit = "49c8559",
-  keys = {
-    {
-      "<leader>mt",
-      function()
-        local mini = require("mini.files")
-        if not mini.close() then
-          mini.open(nil, true)
-        end
-      end,
-      mode = "n",
-      desc = "Mini Files Toggle"
-    },
-    {
-      "<leader>mf",
-      function()
-        local mini = require("mini.files")
-        if not mini.close() then
-          mini.open(vim.api.nvim_buf_get_name(0))
-        end
-      end,
-      mode = "n",
-      desc = "Mini Files Toggle"
-    },
-    {
-      "<leader>mc",
-      function()
-        local mini = require("mini.files")
-        local last_path = mini.get_latest_path()
-        if not mini.close() then
-          if last_path == nil then
-            mini.open(last_path)
-          else
+  {
+    "nvim-mini/mini.files",
+    commit = "49c8559",
+    keys = {
+      {
+        "<leader>mt",
+        function()
+          local mini = require("mini.files")
+          if not mini.close() then
+            mini.open(nil, true)
+          end
+        end,
+        mode = "n",
+        desc = "Mini Files Toggle"
+      },
+      {
+        "<leader>mf",
+        function()
+          local mini = require("mini.files")
+          if not mini.close() then
             mini.open(vim.api.nvim_buf_get_name(0))
           end
-        end
-      end,
-      mode = "n",
-      desc = "Mini Files Toggle CWD"
-    }
+        end,
+        mode = "n",
+        desc = "Mini Files Toggle"
+      },
+      {
+        "<leader>mc",
+        function()
+          local mini = require("mini.files")
+          local last_path = mini.get_latest_path()
+          if not mini.close() then
+            if last_path == nil then
+              mini.open(last_path)
+            else
+              mini.open(vim.api.nvim_buf_get_name(0))
+            end
+          end
+        end,
+        mode = "n",
+        desc = "Mini Files Toggle CWD"
+      }
+    },
+    config = function()
+      local minifiles = require("mini.files")
+      minifiles.setup({})
+    end,
   },
-  config = function()
-    local minifiles = require("mini.files")
-    minifiles.setup({})
-  end,
+  {
+    -- https://github.com/nvim-mini/mini.ai
+    "nvim-mini/mini.ai",
+    commit = "e139eb1",
+    config = function()
+      require("mini.ai").setup()
+    end
+  }
 }
