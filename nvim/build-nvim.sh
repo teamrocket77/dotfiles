047d769b3bfe41e27e50463b423f5512ce42ca0a
vince <vincentcradler@gmail.com>
2026-07-14 23:25:34 -0700

    changed nvim to build v12.4 instead of 11.x

--- nvim/build-nvim.sh
+++ nvim/build-nvim.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 brew install ninja cmake gettext curl
-mkdir -p /tmp/neovim-v11
-git clone -b v0.11.5 https://github.com/neovim/neovim.git /tmp/neovim-v11
-cd /tmp/neovim-v11
+mkdir -p /tmp/neovim
+git clone -b v0.12.4 https://github.com/neovim/neovim.git /tmp/neovim
+cd /tmp/neovim
 
 make CMAKE_BUILD_TYPE=Release DEPS_CMAKE_FLAGS="-DCMAKE_CXX_COMPILER=$(xcrun -find c++)"
 make install
