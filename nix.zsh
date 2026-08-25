82bdc19b9d74981abd32671cf70291c12c8d648b
vince <vincentcradler@gmail.com>
2026-08-24 19:26:00 -0700

    created trap for basic cli stuff

--- nix.zsh
+++ nix.zsh
@@ -1,10 +1,32 @@
-#! /usr/env /bin/zsh
+#!/usr/bin/env zsh
+
+# cannot use -eu b/c the shell plugin scripts fail
+# might fork idk rn
+
+set -E
+
+TRAPZERR() {
+  local last_caller="${funcfiletrace[1]}"
+  
+  # Ignore errors originated from prompt plugins or interactive line redraws
+  if [[ "$last_caller" == *pure* || "$last_caller" == *prompt_* ]]; then
+    return 0
+  fi
+
+  local exit_code=$?
+  echo "[ERR TRAP] Error code $exit_code occurred!" >&2
+  echo "Stack trace:" >&2
+  local frame
+  for frame in "${funcfiletrace[@]}"; do
+    echo "  -> at $frame" >&2
+  done
+
+  return 0
+}
 
 if [[ -d "$HOME/dotfiles/shell" ]]; then
     for F in ~/dotfiles/shell/*(.N); do
-        if ! source "$F"; then
-            echo "Error sourcing: $F"
-        fi
+		source $F
     done
 fi
 
